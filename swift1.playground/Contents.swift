54bf871efa3e4cad73fc332fb26b76c30b532ff0
zhuwenge123 <admin@FanydeiMac13.local>
2018-09-12 11:50:24 +0800

    swift 1..100
    1加到100

---
+++ swift1.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var a=0
+for i in 1...100{
+    a+=i
+    print(i)
+    
+}
+print(a)
+
+
+
+
+
